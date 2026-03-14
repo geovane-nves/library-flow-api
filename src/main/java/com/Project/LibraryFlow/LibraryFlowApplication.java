034a97954185494b4d1ed783d60af4c3bf131e6f
Geovane <geonevesrodrigues@gmail.com>
2026-03-14 10:42:18 -0300

    first commit

---
+++ src/main/java/com/Project/LibraryFlow/LibraryFlowApplication.java
@@ -0,0 +1,13 @@
+package com.Project.LibraryFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryFlowApplication.class, args);
+	}
+
+}
